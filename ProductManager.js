6c37a58a53f8067d0ebd6087dc44b2f66ee383a0
pedrisdm87 <117787470+pedrisdm87@users.noreply.github.com>
2023-08-07 23:39:19 -0300

    feat: agregando mas prods

--- ProductManager.js
+++ ProductManager.js
@@ -110,7 +110,7 @@ const pm = new ProductManager();
     'Biografia',
     2500,
     'Movie.jpg',
-    1,
+    301,
     200
   );
   await pm.addProduct(
@@ -118,9 +118,97 @@ const pm = new ProductManager();
     'Infantil',
     2500,
     'Movie.jpg',
-    2,
+    302,
     200
   );
+  await pm.addProduct(
+    'El Padrino',
+    'Drama',
+    2500,
+    'Movie1.jpg',
+    303,
+    200
+  );
+  await pm.addProduct(
+    'Titanic',
+    'Romance',
+    2500,
+    'Movie2.jpg',
+    304,
+    200
+  );
+  await pm.addProduct(
+    'La Guerra de las Galaxias',
+    'Ciencia Ficción',
+    2500,
+    'Movie3.jpg',
+    305,
+    200
+  );
+
+  await pm.addProduct(
+    'Jurassic Park',
+    'Aventura',
+    2500,
+    'Movie4.jpg',
+    306,
+    200
+  );
+
+  await pm.addProduct(
+    'El Señor de los Anillos',
+    'Fantasía',
+    2500,
+    'Movie5.jpg',
+    307,
+    200
+  );
+
+  await pm.addProduct(
+    'Harry Potter y la Piedra Filosofal',
+    'Fantasía',
+    2500,
+    'Movie6.jpg',
+    308,
+    200
+  );
+
+  await pm.addProduct(
+    'Avatar',
+    'Ciencia Ficción',
+    2500,
+    'Movie7.jpg',
+    309,
+    200
+  );
+
+  await pm.addProduct(
+    'El Rey León',
+    'Infantil',
+    2500,
+    'Movie8.jpg',
+    310,
+    200
+  );
+
+  await pm.addProduct(
+    'Forrest Gump',
+    'Drama',
+    2500,
+    'Movie9.jpg',
+    311,
+    200
+  );
+
+  await pm.addProduct(
+    'Misión Imposible',
+    'Acción',
+    2500,
+    'Movie10.jpg',
+    312,
+    200
+  );
+
 
   console.log(await pm.getProducts());
 })();
